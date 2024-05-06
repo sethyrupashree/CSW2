8c30091db2a978ade6a1b7d8111ceb4b33e9d458
sethyrupashree <147615646+sethyrupashree@users.noreply.github.com>
2024-05-06 15:13:09 +0530

    Add files via upload

---
+++ Assignment_3/q28.java
@@ -0,0 +1,15 @@
+
+public class q28 {
+    public static void main(String[] args) {
+        try {
+            recursiveMethod(1);
+        } catch (StackOverflowError e) {
+            System.out.println("Caught StackOverflowError: " + e.getMessage());
+        }
+    }
+
+    public static void recursiveMethod(int i) {
+        System.out.println("Current value of i: " + i);
+        recursiveMethod(i + 1);
+    }
+}
